772d2308cff6caad53448a6784d95dda3b51d252
agusL1660 <lelliag@hotmail.com>
2020-09-22 11:20:37 -0300

    Commit de conflicto

--- Prueba.java
+++ Prueba.java
@@ -2,7 +2,7 @@ public class Prueba {
 	
 	public static void main (String[] args){
 		Automovil a=new Automovil("Fiat","147");
-		Persona p = new Persona("Lionel","Messi",a);
+		Persona p = new Persona("Lautaro","Martinez",a);
 		a.setDueño(p);
 		System.out.println("Nombre: "+p.getName()+" Apellido: "+p.getApellido()+ "Automovil: "+a.getMarca()+" "+a.getSerie());
 
